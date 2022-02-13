aab8e0bff143a92357b011ce198a66aacfb52ddc
Welberty Martins <38479002+welbertymartins@users.noreply.github.com>
2022-02-13 16:12:22 -0300

    feat: add v1.0.7

--- cryptoMiddleware.js
+++ cryptoMiddleware.js
@@ -1,25 +1,51 @@
 const crypto = require("crypto")
 
 const crypt = (algorithm = "aes-256-ctr") => (privateKey) => (content) => {
-    const cipher = crypto.createCipher(algorithm, privateKey)
-    return cipher.update(content, "utf8", "hex")
+    try {
+        const cipher = crypto.createCipher(algorithm, privateKey)
+        return cipher.update(content, "utf8", "hex")
+    } catch (error) {
+        console.log(error)
+        return ``
+    }
 }
 
 const decrypt = (algorithm = "aes-256-ctr") => (privateKey) => (content) => {
-    const decipher = crypto.createDecipher(algorithm, privateKey)
-    return decipher.update(content, "hex", "utf8")
+    try {
+        const decipher = crypto.createDecipher(algorithm, privateKey)
+        return decipher.update(content, "hex", "utf8")
+    } catch (error) {
+        console.log(error)
+        return ``
+    }
 }
 
-const hash = (algorithm = "sha512") => (content) => crypto.createHash(algorithm).update(content).digest("hex")
+const hash = (algorithm = "sha512") => (content) => {
+    try {
+        return crypto.createHash(algorithm).update(content).digest("hex")
+    } catch (error) {
+        console.log(error)
+        return ``
+    }
+}
 
 const base64Encode = (content) => {
-    const bufferObj = Buffer.from(content, "utf8")
-    return bufferObj.toString("base64")
+    try {
+        const bufferObj = Buffer.from(content, "utf8")
+        return bufferObj.toString("base64")
+    } catch (error) {
+        console.log(error)
+        return ``
+    }
 }
 
 const base64Decode = (content) => {
-    const bufferObj = Buffer.from(content, "base64")
-    return bufferObj.toString("utf8")
+    try {
+        const bufferObj = Buffer.from(content, "base64")
+        return bufferObj.toString("utf8")
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = { crypt, decrypt, hash, base64Encode, base64Decode }
--- test/cryptoMiddleware.spec.js
+++ test/cryptoMiddleware.spec.js
@@ -1,13 +1,13 @@
 const expect = require("chai").expect
 const cryptoMiddleware = require("../cryptoMiddleware")
 
-describe("Crypto Middleware", function() {
+describe("Crypto Middleware", function () {
 
     it("Encode and Decode Text", function () {
         const text = "Hello World"
         const base64Encode = cryptoMiddleware.base64Encode
         const base64Decode = cryptoMiddleware.base64Decode
-        expect( base64Decode(base64Encode(text)) ).to.equal(text)
+        expect(base64Decode(base64Encode(text))).to.equal(text)
     })
 
     it("Crypt and Decrypt Text", function () {
@@ -15,13 +15,13 @@ describe("Crypto Middleware", function() {
         const password = 'password'
         const crypt = cryptoMiddleware.crypt()(password)
         const decrypt = cryptoMiddleware.decrypt()(password)
-        expect( decrypt(crypt(text)) ).to.equal(text)
+        expect(decrypt(crypt(text))).to.equal(text)
     })
 
     it("Hash Text", function () {
         const text = "Hello World"
         const textHash = "2c74fd17edafd80e8447b0d46741ee243b7eb74dd2149a0ab1b9246fb30382f27e853d8585719e0e67cbda0daa8f51671064615d645ae27acb15bfb1447f459b"
         const hash = cryptoMiddleware.hash()
-        expect( hash(text) ).to.equal(textHash)
+        expect(hash(text)).to.equal(textHash)
     })
 })
